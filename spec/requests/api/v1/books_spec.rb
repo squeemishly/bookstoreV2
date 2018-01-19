3a9b55b5bddfefd76fce6212792247a16d5f294e
squeemishly <squeemishly@gmail.com>
2018-01-18 21:21:01 -0700

    add test for moving books between locations

--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -18,7 +18,6 @@ describe 'Bookstore API' do
     expect(book[:id]).to eq book_id
     expect(book[:title]).to eq "fake title"
     expect(book[:genre]).to eq "fake genre"
-    expect(book[:locations]).to be_a Array
     expect(book[:locations].first).to eq "fake location"
   end
 
@@ -30,10 +29,23 @@ describe 'Bookstore API' do
     expect(response).to be_success
     expect(location[:id]).to eq location_id
     expect(location[:name]).to eq "fake location"
-    expect(location[:books]).to be_a Array
-    expect(location[:books].first).to be_a Hash
     expect(location[:books].first[:id]).to eq book_id
     expect(location[:books].first[:title]).to eq "fake title"
     expect(location[:books].first[:genre]).to eq "fake genre"
+  end
+
+  it 'can move books from one location to another' do
+    new_location_id = create(:location, name: "fake second location").id
+
+    put "/api/v1/books/#{book_id}/locations/#{new_location_id}"
+
+    book = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_success
+    expect(book[:id]).to eq book_id
+    expect(book[:title]).to eq "fake title"
+    expect(book[:genre]).to eq "fake genre"
+    expect(book[:locations].first).to eq "fake second location"
+    expect(book[:locations].first).to_not eq "fake location"
   end
 end
