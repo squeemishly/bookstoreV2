bde5ac1554f663dd34f4470cb69c8918b02d1cc2
squeemishly <squeemishly@gmail.com>
2018-01-18 20:03:14 -0700

    return location information and books in the location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,26 @@
 class Location < ApplicationRecord
   has_many :book_locations
   has_many :books, through: :book_locations
+
+  def self.find_books(id)
+    location = Location.find(id)
+
+    {
+      id: location.id,
+      name: location.name,
+      books: location_books(location)
+    }
+  end
+
+  private
+
+  def self.location_books(location)
+    location.books.map do |book|
+      {
+        id: book.id,
+        title: book.title,
+        genre: book.genre
+      }
+    end
+  end
 end
