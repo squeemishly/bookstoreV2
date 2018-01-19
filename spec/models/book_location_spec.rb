5d1bfa22ce4361e8aeae6c0961eb76a55567a92c
squeemishly <squeemishly@gmail.com>
2018-01-18 21:21:55 -0700

    write test for BookLocation.move_book

--- spec/models/book_location_spec.rb
+++ spec/models/book_location_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe BookLocation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BookLocation, type: :model do
+  describe '.move_book' do
+    it 'updates a books location' do
+      allow(Book).to receive(:find_book).and_return(:fake_book_information)
+
+      book_id = create(:book).id
+      location_id = create(:location).id
+      book_location = create(:book_location, book_id: book_id, location_id: location_id)
+      new_location_id = create(:location, name: "fake second location").id
+
+      result = BookLocation.move_book(book_id, location_id)
+
+      expect(result).to eq :fake_book_information
+    end
+  end
 end
