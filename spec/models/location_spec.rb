fe9750a07ab9fe895008f4b827836b4df7cfbe58
squeemishly <squeemishly@gmail.com>
2018-01-18 20:10:24 -0700

    write test for .find_books

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Location, type: :model do
+  describe ".find_books" do
+    it 'returns a hash of location information and books in that location' do
+      book_id = create(:book).id
+      location_id = create(:location).id
+      book_location = create(:book_location, book_id: book_id, location_id: location_id)
+
+      result = Location.find_books(location_id)
+
+      expect(result).to be_a Hash
+      expect(result).to have_key :id
+      expect(result).to have_key :name
+      expect(result).to have_key :books
+      expect(result[:books]).to be_an Array
+      expect(result[:books].first).to have_key :id
+      expect(result[:books].first).to have_key :title
+      expect(result[:books].first).to have_key :genre
+
+      expect(result[:id]).to eq location_id
+      expect(result[:name]).to eq "fake location"
+      expect(result[:books].first[:id]).to eq book_id
+      expect(result[:books].first[:title]).to eq "fake title"
+      expect(result[:books].first[:genre]).to eq "fake genre"
+    end
+  end
 end
