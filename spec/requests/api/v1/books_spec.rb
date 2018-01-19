06b2fd52a327dc1c71634a0e9d8523b2c8c03951
squeemishly <squeemishly@gmail.com>
2018-01-18 18:21:17 -0700

    use symbols for test, update response to include locations

--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe 'Bookstore API' do
-  it 'can get one book by id and show the location' do
+  it 'can get one book by id and show the locations' do
     book_id = create(:book).id
     location_id = create(:location).id
     book_location = create(:book_location, book_id: book_id, location_id: location_id)
 
     get "/api/v1/books/#{book_id}"
 
-    book = JSON.parse(response.body)
+    book = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_success
-    expect(book["id"]).to eq book_id
-    expect(book["title"]).to eq "fake title"
-    expect(book["genre"]).to eq "fake genre"
-    expect(book["location"]).to eq "fake location"
+    expect(book[:id]).to eq book_id
+    expect(book[:title]).to eq "fake title"
+    expect(book[:genre]).to eq "fake genre"
+    expect(book[:locations]).to eq ["fake location"]
   end
 end
